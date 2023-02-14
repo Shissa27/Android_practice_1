a872daebd9bc99da73144679e8bdd5adf472041f
Artem <th37et@gmail.com>
2023-02-14 19:47:14 +0300

    Toasts added

--- app/src/main/java/com/example/practica_1/MainActivity.java
+++ app/src/main/java/com/example/practica_1/MainActivity.java
@@ -2,13 +2,48 @@ package com.example.practica_1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    int duration = Toast.LENGTH_SHORT;
+    Context context = MainActivity.this;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast toast = Toast.makeText(context, "Toast_onCreate", duration);
+        toast.show();
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast toast = Toast.makeText(context, "Toast_onStart", duration);
+        toast.show();
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast toast = Toast.makeText(context, "Toast_onStop", duration);
+        toast.show();
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast toast = Toast.makeText(context, "Toast_onDestroy", duration);
+        toast.show();
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast toast = Toast.makeText(context, "Toast_onPause", duration);
+        toast.show();
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast toast = Toast.makeText(context, "Toast_onResume", duration);
+        toast.show();
     }
 }
