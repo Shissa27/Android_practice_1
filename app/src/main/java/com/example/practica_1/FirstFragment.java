69d1ac25d7aa1fa4b8a76f34f33f9fc115f107a5
Artem <th37et@gmail.com>
2023-03-02 18:46:11 +0300

    Practice 3.2

--- app/src/main/java/com/example/practica_1/FirstFragment.java
+++ app/src/main/java/com/example/practica_1/FirstFragment.java
@@ -1,19 +1,93 @@
 package com.example.practica_1;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 
 public class FirstFragment extends Fragment {
+    String TAG = "Fragment 1";
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        Toast.makeText(context, "onAttach", Toast.LENGTH_SHORT).show();
+        Log.d(TAG, "onAttach");
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Toast.makeText(getActivity(), "onCreate", Toast.LENGTH_SHORT).show();
+        Log.d(TAG, "onCreate");
+    }
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+        Toast.makeText(getActivity(), "onCreateView", Toast.LENGTH_SHORT).show();
+        Log.d(TAG, "onCreateView");
         return inflater.inflate(R.layout.fragment_first, container, false);
     }
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        Toast.makeText(getActivity(), "onActivityCreated", Toast.LENGTH_SHORT).show();
+        Log.d(TAG, "onActivityCreated");
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Toast.makeText(getActivity(), "onStart", Toast.LENGTH_SHORT).show();
+        Log.d(TAG, "onStart");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Toast.makeText(getActivity(), "onResume", Toast.LENGTH_SHORT).show();
+        Log.d(TAG, "onResume");
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Toast.makeText(getActivity(), "onPause", Toast.LENGTH_SHORT).show();
+        Log.d(TAG, "onPause");
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Toast.makeText(getActivity(), "onStop", Toast.LENGTH_SHORT).show();
+        Log.d(TAG, "onStop");
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        Toast.makeText(getActivity(), "onDestroyView", Toast.LENGTH_SHORT).show();
+        Log.d(TAG, "onDestroyView");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(getActivity(), "onDestroy", Toast.LENGTH_SHORT).show();
+        Log.d(TAG, "onDestroy");
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        Toast.makeText(getActivity(), "onDetach", Toast.LENGTH_SHORT).show();
+        Log.d(TAG, "onDetach");
+    }
+
 }
--- app/src/main/java/com/example/practica_1/SecondFragment.java
+++ app/src/main/java/com/example/practica_1/SecondFragment.java
@@ -1,18 +1,36 @@
 package com.example.practica_1;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 public class SecondFragment extends Fragment {
+    String TAG = "Fragment 2";
     @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        Toast.makeText(context, "onAttach", Toast.LENGTH_SHORT).show();
+        Log.d(TAG, "onAttach");
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Toast.makeText(getActivity(), "onCreate", Toast.LENGTH_SHORT).show();
+        Log.d(TAG, "onCreate");
+    }
+   @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
+        Toast.makeText(getActivity(), "onCreateView", Toast.LENGTH_SHORT).show();
+        Log.d(TAG, "onCreateView");
         return inflater.inflate(R.layout.fragment_second, container, false);
     }
 }
