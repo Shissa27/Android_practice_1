41b44560a339f8e935e4ca5498041c2e5d016537
Artem <th37et@gmail.com>
2023-03-17 14:13:15 +0300

    Practice 4.0

--- app/src/main/java/com/example/practica_1/FirstFragment.java
+++ app/src/main/java/com/example/practica_1/FirstFragment.java
@@ -21,98 +21,11 @@ import com.example.practica_1.databinding.FragmentFirstBinding;
 public class FirstFragment extends Fragment {
     String TAG = "Fragment 1";
 
-        @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        Toast.makeText(context, "onAttach", Toast.LENGTH_SHORT).show();
-        Log.d(TAG, "onAttach");
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        Toast.makeText(getActivity(), "onCreate", Toast.LENGTH_SHORT).show();
-        Log.d(TAG, "onCreate");
-
-    }
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        Toast.makeText(getActivity(), "onCreateView", Toast.LENGTH_SHORT).show();
-        Log.d(TAG, "onCreateView");
-        View view = inflater.inflate(R.layout.fragment_first, container, false);
-        Button button = (Button) view.findViewById(R.id.get_location_first_fragment);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                EditText editText = (EditText) view.findViewById(R.id.get_coordinate);
-                Bundle result = new Bundle();
-                result.putString("bundleKey", String.valueOf(editText.getText()));
-                getParentFragmentManager().setFragmentResult(
-                        "requestKey", result);
-                FragmentManager fragmentManager = getParentFragmentManager();
-                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-                fragmentTransaction.replace(R.id.frame, new SecondFragment());
-                fragmentTransaction.addToBackStack(null);
-                fragmentTransaction.commit();
-            }
-        });
-        return view;
-    }
-    @Override
-    public void onActivityCreated(Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        Toast.makeText(getActivity(), "onActivityCreated", Toast.LENGTH_SHORT).show();
-        Log.d(TAG, "onActivityCreated");
+
+        return inflater.inflate(R.layout.fragment_second, container, false);
     }
 
-    @Override
-    public void onStart() {
-        super.onStart();
-        Toast.makeText(getActivity(), "onStart", Toast.LENGTH_SHORT).show();
-        Log.d(TAG, "onStart");
-
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        Toast.makeText(getActivity(), "onResume", Toast.LENGTH_SHORT).show();
-        Log.d(TAG, "onResume");
-    }
-
-    @Override
-    public void onPause() {
-        super.onPause();
-        Toast.makeText(getActivity(), "onPause", Toast.LENGTH_SHORT).show();
-        Log.d(TAG, "onPause");
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-        Toast.makeText(getActivity(), "onStop", Toast.LENGTH_SHORT).show();
-        Log.d(TAG, "onStop");
-    }
-
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        Toast.makeText(getActivity(), "onDestroyView", Toast.LENGTH_SHORT).show();
-        Log.d(TAG, "onDestroyView");
-    }
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        Toast.makeText(getActivity(), "onDestroy", Toast.LENGTH_SHORT).show();
-        Log.d(TAG, "onDestroy");
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        Toast.makeText(getActivity(), "onDetach", Toast.LENGTH_SHORT).show();
-        Log.d(TAG, "onDetach");
-    }
 
 }
--- app/src/main/java/com/example/practica_1/SecondFragment.java
+++ app/src/main/java/com/example/practica_1/SecondFragment.java
@@ -18,88 +18,9 @@ import com.example.practica_1.databinding.FragmentSecondBinding;
 
 public class SecondFragment extends Fragment {
     String TAG = "Fragment 2";
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        Toast.makeText(context, "onAttach", Toast.LENGTH_SHORT).show();
-        Log.d(TAG, "onAttach");
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        Toast.makeText(getActivity(), "onCreate", Toast.LENGTH_SHORT).show();
-        Log.d(TAG, "onCreate");
-
-        getParentFragmentManager().setFragmentResultListener("requestKey", this, new FragmentResultListener() {
-            public void onFragmentResult(@NonNull String requestKey, @NonNull Bundle bundle) {
-                String result = bundle.getString("bundleKey");
-                Toast.makeText(getActivity(), result, Toast.LENGTH_SHORT).show();
-                Log.i(TAG,"Got_info");
-            }
-        });
-    }
-   @Override
+     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        Toast.makeText(getActivity(), "onCreateView", Toast.LENGTH_SHORT).show();
-        Log.d(TAG, "onCreateView");
         return inflater.inflate(R.layout.fragment_second, container, false);
     }
-    @Override
-    public void onActivityCreated(Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        Toast.makeText(getActivity(), "onActivityCreated", Toast.LENGTH_SHORT).show();
-        Log.d(TAG, "onActivityCreated");
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-        Toast.makeText(getActivity(), "onStart", Toast.LENGTH_SHORT).show();
-        Log.d(TAG, "onStart");
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        Toast.makeText(getActivity(), "onResume", Toast.LENGTH_SHORT).show();
-        Log.d(TAG, "onResume");
-    }
-
-    @Override
-    public void onPause() {
-        super.onPause();
-        Toast.makeText(getActivity(), "onPause", Toast.LENGTH_SHORT).show();
-        Log.d(TAG, "onPause");
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-        Toast.makeText(getActivity(), "onStop", Toast.LENGTH_SHORT).show();
-        Log.d(TAG, "onStop");
-    }
-
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        Toast.makeText(getActivity(), "onDestroyView", Toast.LENGTH_SHORT).show();
-        Log.d(TAG, "onDestroyView");
-    }
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        Toast.makeText(getActivity(), "onDestroy", Toast.LENGTH_SHORT).show();
-        Log.d(TAG, "onDestroy");
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        Toast.makeText(getActivity(), "onDetach", Toast.LENGTH_SHORT).show();
-        Log.d(TAG, "onDetach");
-    }
-
 }
