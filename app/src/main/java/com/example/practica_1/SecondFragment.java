76afc80928ced0107945ca9110a61a23e34e9f26
Artem <th37et@gmail.com>
2023-03-18 11:33:42 +0300

    Practice 4.3 Final

--- app/src/main/java/com/example/practica_1/SecondFragment.java
+++ app/src/main/java/com/example/practica_1/SecondFragment.java
@@ -14,9 +14,11 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.practica_1.databinding.FragmentSecBinding;
 import com.example.practica_1.databinding.FragmentSecondBinding;
 
 import java.util.ArrayList;
@@ -26,18 +28,37 @@ import java.util.Random;
 public class SecondFragment extends Fragment {
     String TAG = "Fragment 2";
 
-
-
-
+    FragmentSecondBinding binding;
+    public String generateRandomAstra() {
+        String letters = "abcdefghijklmnopqrstuvwxyz";
+        Random rand = new Random();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < 3; i++) {
+            sb.append(letters.charAt(rand.nextInt(letters.length())));
+        }
+        for (int i = 0; i < 2; i++) {
+            sb.append(rand.nextInt(10));
+        }
+        return sb.toString();
+    }
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        final Random random = new Random();
+        List<String> Stars =new ArrayList<>();
+        for (int i = 0; i < 200; i++) {
+            int num = random.nextInt(700);
+            String star = generateRandomAstra();
+            Stars.add("Звезда " + star);
+        }
+        Log.i("PUDGE",Stars.size()+"A");
+        CustomViewListAdapter viewListAdapter = new CustomViewListAdapter(binding.getRoot().getContext(),R.layout.vlist_orig,Stars);
+        binding.vlist.setAdapter(viewListAdapter);
+    }
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        FragmentManager fragmentManager = getParentFragmentManager();
-        FragmentTransaction fragmentTransaction;
-        fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.replace(R.id.frame2,new FragmentSec());
-        fragmentTransaction.commit();
-        return inflater.inflate(R.layout.fragment_second, container, false);
+        binding = FragmentSecondBinding.inflate(getLayoutInflater());
+        return binding.getRoot();
     }
 
 }
